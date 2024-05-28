53e5bb820db2b315fabf084498dce59d49a6e8e3
Mallart <matheo.allart09@gmail.com>
2024-05-28 18:01:10 +0200

    Fixed program closing on hiding descriptions windows

--- src/main/java/ihm/FromageDescription.java
+++ src/main/java/ihm/FromageDescription.java
@@ -57,7 +57,7 @@ public class FromageDescription extends JFrame {
 	 * Create the frame.
 	 */
 	public FromageDescription(Fromage fromage) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 684, 406);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
