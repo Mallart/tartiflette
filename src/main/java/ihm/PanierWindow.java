2d79b6996663eeadac8dc5899a16d285648cf7f1
Mallart <matheo.allart09@gmail.com>
2024-06-04 17:51:27 +0200

    made the window closeable without shutting down the app

--- src/main/java/ihm/PanierWindow.java
+++ src/main/java/ihm/PanierWindow.java
@@ -70,7 +70,7 @@ public class PanierWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public PanierWindow(Panier panier) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
