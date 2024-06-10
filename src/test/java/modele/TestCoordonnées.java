4e73534e1278995f57173fa19dac16d1eadc0669
Diego Massat <diego.massat@etu.iut-tlse3.fr>
2024-06-10 08:25:17 +0200

    rajout de testCoordonnées
    h

---
+++ src/test/java/modele/TestCoordonnées.java
@@ -0,0 +1,12 @@
+package modele;
+
+import org.junit.Test;
+
+public class TestCoordonnées {
+
+	@Test
+	public void TestCoordonnéesFromage() {
+		Coordonnées DesCoordonnées = new Coordonnées(null, null, null, null, null, null, null, null, null, false);
+	}
+
+}
